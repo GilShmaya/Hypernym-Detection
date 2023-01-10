e89da4812b2c2aa17e8b4f09c018d5302d22b173
gilshmaya <t-gilshmay@microsoft.com>
2023-01-10 23:10:02 +0200

    implement FeaturesVectorBuilder reducer

--- FeaturesVectorBuilder/src/main/java/FeaturesVectorBuilder.java
+++ FeaturesVectorBuilder/src/main/java/FeaturesVectorBuilder.java
@@ -1,11 +1,21 @@
+import com.google.common.base.Joiner;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.BooleanWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Partitioner;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.tartarus.snowball.ext.englishStemmer;
+import utils.FeaturesVectorLength;
 import utils.PairOfNouns;
 import utils.PatternInfo;
 
@@ -16,7 +26,7 @@ import java.io.IOException;
  *     1. Parse the data from the annotated set.
  *     2. Stem the pair.
  *     3. Aggregate the annotated set data & the PatternParser's output.
- *     4.
+ *     4. Create a features vector for each noun pair
  */
 public class FeaturesVectorBuilder {
 
@@ -50,7 +60,7 @@ public class FeaturesVectorBuilder {
             englishStemmer.stem();
             String word2 = englishStemmer.getCurrent();
 
-            BooleanWritable isHypernym = new BooleanWritable(Boolean.parseBoolean(arr[2]);
+            BooleanWritable isHypernym = new BooleanWritable(Boolean.parseBoolean(arr[2]));
             PairOfNouns pairOfNouns =
                     new PairOfNouns(new Text(word1), new Text(word2), isHypernym, new IntWritable(-1));
             context.write(pairOfNouns, new PatternInfo(new IntWritable(-1), new IntWritable(-1)));
@@ -59,7 +69,8 @@ public class FeaturesVectorBuilder {
 
 
     /***
-     * * Defines the partition policy of sending the key-value the Mapper created to the reducers.
+     * * Defines the partition policy of sending the key-value the Mapper created to the reducers. The annotated pair
+     * * should arrive before PatternsParser's output
      */
     public static class PartitionerClass extends Partitioner<PairOfNouns, PatternInfo> { // TODO: remove?
         public int getPartition(PairOfNouns key, PatternInfo value, int numPartitions) {
@@ -70,59 +81,62 @@ public class FeaturesVectorBuilder {
 
     // TODO
     public static class ReducerClass extends Reducer<PairOfNouns, PatternInfo, Text, Text> {
-        private String BUCKET_NAME;
-        private PairOfNouns currPairOfNouns = null;
-        int totalFeatures;
+        private PairOfNouns currPairOfNouns;
+        private int featuresVectorLength;
 
         @Override
         public void setup(Context context) throws IOException, InterruptedException {
             super.setup(context);
-            BUCKET_NAME = context.getConfiguration().get("BUCKET_NAME");
-            S3Client s3 = S3Client.builder().region(Region.US_EAST_1).build();
-            ListObjectsRequest listObjects = ListObjectsRequest
-                    .builder()
-                    .bucket(BUCKET_NAME)
-                    .prefix("totals/")
-                    .build();
-
-            ListObjectsResponse res = s3.listObjects(listObjects);
-            List<S3Object> objects = res.contents();
-            for (ListIterator iterVals = objects.listIterator(); iterVals.hasNext(); ) {
-                S3Object myValue = (S3Object) iterVals.next();
-                String[] fileName = myValue.key().split("_");
-                if (fileName.length > 2)
-                    this.totalFeatures = Integer.parseInt(fileName[1]);
-            }
+            this.currPairOfNouns = null;
+            this.featuresVectorLength = context.getConfiguration().getInt("Length", 0);
         }
 
-        public void reduce(PairOfNouns key, Iterable<PatternInfo> values, Context context) throws IOException, InterruptedException {
+        public void reduce(PairOfNouns key, Iterable<PatternInfo> values,
+                           Context context) throws IOException, InterruptedException {
             if (key.getTotal().get() == -1) { // annotated pair
-//                this.pairOfNouns =  new PairOfNouns(key.getWord1(), key.getWord2(), key.getIsHypernym(), new
-//                IntWritable(-1));
-                this.currPairOfNouns = key;
+                currPairOfNouns = key;
+            } else if (this.currPairOfNouns != null && currPairOfNouns.getWord1().equals(key.getWord1()) &&
+                    // PatternsParser's output
+                    currPairOfNouns.getWord2().equals(key.getWord2())) {
+
+                Long[] featuresVector = new Long[this.featuresVectorLength]; // initialize features vector
+                for (int i = 0; i < this.featuresVectorLength; i++) {
+                    featuresVector[i] = 0L;
+                }
+
+                for (PatternInfo patternInfo : values) { // fill features vector with pattern occurrences
+                    if (patternInfo.getPatternIndex().get() < featuresVectorLength) {
+                        featuresVector[patternInfo.getPatternIndex().get()] += patternInfo.getOccurrences().get();
+                    }
+                }
+
+                context.write(new Text(String.format("%s %s",
+                                currPairOfNouns.getWord1().toString(), currPairOfNouns.getWord2().toString())),
+                        new Text(String.format("Hypernym: %s featuresVector: %s",
+                                currPairOfNouns.getIsHypernym().get(), Joiner.on(",").join(featuresVector))));
             }
-            if (this.currPairOfNouns == null || !key.getWord1().toString().equals(this.currPairOfNouns.getWord1().toString()) || !key.getWord2().toString().equals(this.currPairOfNouns.getWord2().toString())) {
-                System.err.println("NEED TO RETURN");
-                return;
-            }
-            System.err.println("FILL VECTOR of " + key.getW1() + " " + key.getW2());
-            Long[] featuresVector = new Long[this.totalFeatures];
-            for (int i = 0; i < this.totalFeatures; i++) {
-                featuresVector[i] = new Long(0);
-            }
-            for (TotalPatternPair pattern : values) {
-                System.err.println("PATTERN " + pattern.getPattern().get());
-                System.err.println("TOTAL COUNT " + pattern.getTotal_count().get());
-                featuresVector[pattern.getPattern().get()] += pattern.getTotal_count().get();
-            }
-            Text val = new Text(StringUtils.join(featuresVector, ','));
-            System.err.println("WRITE " + key.toString() + " CURPAIR: " + this.currPairOfNouns.toString());
-            context.write(new Text(key.getW1().toString() + " " + key.getW2().toString()), new Text(this.currPairOfNouns.getIsHypernym().get() + "," + val));
         }
     }
 
     public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        long featuresVectorLength = FeaturesVectorLength.getInstance().getLength();
+        conf.setLong("Length", featuresVectorLength);
+        Job job = Job.getInstance(conf, "FeaturesVectorBuilder");
+        job.setJarByClass(FeaturesVectorBuilder.class);
+        job.setNumReduceTasks(1);
+        MultipleInputs.addInputPath(job, new Path(.BUCKET_PATH + "/Step1"), SequenceFileInputFormat.class,
+                MapperClass.class);
+        MultipleInputs.addInputPath(job, new Path(MainLogic.BUCKET_PATH + "/ANNOTATED_SET"), TextInputFormat.class, MapperClassAnnotated.class);
+        job.setReducerClass(ReducerClass.class);
+        job.setMapOutputKeyClass(PairOfNouns.class);
+        job.setMapOutputValueClass(PatternInfo.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+        FileOutputFormat.setOutputPath(job, new Path(MainLogic.BUCKET_PATH + "/Step2"));
+        job.setOutputFormatClass(TextOutputFormat.class);
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 
 
-    }
+}
